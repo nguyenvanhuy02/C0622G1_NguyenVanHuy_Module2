c15802cfbfa6351478a4596ed90137f457a1c4aa
nguyenvanhuy02 <maohuy123312@gmail.com>
2022-08-12 08:27:53 +0700

    cập nhật

--- src/ss8_clean_code_refactoring/exercise/refactoring/TennisGame.java
+++ src/ss8_clean_code_refactoring/exercise/refactoring/TennisGame.java
@@ -7,62 +7,71 @@ public class TennisGame {
     private static final int SCORE_2 = 2;
     private static final int SCORE_3 = 3;
 
-    public static String getScore(int firstScorePlayer, int secondScorePlayer) {
+    public static void getScore(int firstScorePlayer, int secondScorePlayer) {
+        String score = "";
+        if (firstScorePlayer == secondScorePlayer) {
+            System.out.println(draw(firstScorePlayer));
+        } else if (firstScorePlayer >= 4 || secondScorePlayer >= 4) {
+            System.out.println(playWin(firstScorePlayer,secondScorePlayer));
+        } else {
+            System.out.println(playWin2(firstScorePlayer,secondScorePlayer));
+        }
+    }
+    public static String draw(int firstScorePlayer){
+        switch (firstScorePlayer) {
+            case SCORE_0:
+                return  "Love-All";
+            case SCORE_1:
+                return  "Fifteen-All";
+            case SCORE_2:
+                return  "Thirty-All";
+            case SCORE_3:
+                return  "Forty-All";
+            default:
+                return  "Deuce";
+
+        }
+    }
+
+    public static String playWin(int firstScorePlayer, int secondScorePlayer){
+        int minusResult = firstScorePlayer - secondScorePlayer;
+        if (minusResult == 1)
+           return  "Advantage player1";
+        else if (minusResult == -1)
+            return  "Advantage player2";
+        else if (minusResult >= 2)
+            return  "Win for player1";
+        else
+            return  "Win for player2";
+    }
+
+    public static String playWin2(int firstScorePlayer, int secondScorePlayer) {
         String score = "";
         int tempScore;
-        if (firstScorePlayer == secondScorePlayer) {
-            switch (firstScorePlayer) {
+        for (int i = 1; i < 3; i++) {
+            if (i == 1)
+                tempScore = firstScorePlayer;
+            else {
+                score += "-";
+                tempScore = secondScorePlayer;
+            }
+            switch (tempScore) {
                 case SCORE_0:
-                    score = "Love-All";
+                    score += "Love";
                     break;
                 case SCORE_1:
-                    score = "Fifteen-All";
+                    score += "Fifteen";
                     break;
                 case SCORE_2:
-                    score = "Thirty-All";
+                    score += "Thirty";
                     break;
                 case SCORE_3:
-                    score = "Forty-All";
+                    score += "Forty";
                     break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
-        } else if (firstScorePlayer >= 4 || secondScorePlayer >= 4) {
-            int minusResult = firstScorePlayer - secondScorePlayer;
-            if (minusResult == 1)
-                score = "Advantage player1";
-            else if (minusResult == -1)
-                score = "Advantage player2";
-            else if (minusResult >= 2)
-                score = "Win for player1";
-            else
-                score = "Win for player2";
-        } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1)
-                    tempScore = firstScorePlayer;
-                else {
-                    score += "-";
-                    tempScore = secondScorePlayer;
-                }
-                switch (tempScore) {
-                    case SCORE_0:
-                        score += "Love";
-                        break;
-                    case SCORE_1:
-                        score += "Fifteen";
-                        break;
-                    case SCORE_2:
-                        score += "Thirty";
-                        break;
-                    case SCORE_3:
-                        score += "Forty";
-                        break;
-                }
             }
         }
         return score;
+
     }
+
 }
